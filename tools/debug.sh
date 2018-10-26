dd1ed2c84dcee045aa431d838380c055fe85854c
xafero <georg.berendt@uni-oldenburg.de>
2018-10-27 01:28:07 +0200

    Added skopeo tool

--- tools/debug.sh
+++ tools/debug.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-go run imgview/src/imgview/program.go tcp://192.168.178.47:2375
+echo go run imgview/src/imgview/program.go tcp://192.168.178.47:2375
+go run imgfind/src/imgfind/program.go tcp://192.168.178.47:2375
