eddadea7341baa82e832c2d31399da9cd8939920
xafero <georg.berendt@uni-oldenburg.de>
2018-10-27 17:07:23 +0200

    Did scripts

--- tools/debug.sh
+++ tools/debug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo go run imgview/src/imgview/program.go tcp://192.168.178.47:2375
+go run imgview/src/imgview/program.go tcp://192.168.178.47:2375
 go run imgfind/src/imgfind/program.go openjdk tcp://192.168.178.47:2375
