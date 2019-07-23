6abf8c0d8b40da8f81ee8d738392d6777c2929ef
xafero <georg.berendt@uni-oldenburg.de>
2019-07-23 14:00:36 +0200

    Added certificate creation with OpenSSL

---
+++ scripts/linux/secure/create-cert.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+openssl req -config https.config -new -out csr.pem
+openssl x509 -req -days 365 -extfile https.config -extensions v3_req -in csr.pem -signkey key.pem -out https.crt
+openssl pkcs12 -export -out https.pfx -inkey key.pem -in https.crt
